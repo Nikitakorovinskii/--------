69a01a0dfd51fdd28fd2b3412aab3a937ee60850
Nikitakorovinskii <78597438+Nikitakorovinskii@users.noreply.github.com>
2022-02-09 09:52:15 +0300

    first commit

---
+++ VU/Name.js
@@ -0,0 +1,12 @@
+        const App = Vue.createApp({
+            data() {
+                return {
+                    firstName: 'Nikita',
+                    lastName: 'Korovinskii',
+                    middleName: 'Sergeevich'
+                };
+            }
+        });
+        // Registering and mounting our app
+        App.mount('#app');
+    
